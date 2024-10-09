815b273b0847d724fc987ac373c46b3e2d4e59f7
Graps <twinklierre@gmail.com>
2024-10-09 20:15:21 +0700

    #5 modified DataCollector to open and close file only once

--- src/data_collector.py
+++ src/data_collector.py
@@ -8,12 +8,17 @@ INTERVAL = 0.5
 
 class DataCollector(GameObject):
     def __init__(self, filename: str, ball_key: str, paddle_key: str) -> None:
-        self.filename = filename
         self.counter = 0
         self.ball_key = ball_key
         self.paddle_key = paddle_key
         self.ball: Ball = None
         self.paddle: Paddle = None
+
+        self.file = open(filename, 'a', newline='')
+        self.writer = csv.writer(self.file)
+
+    def __del__(self):
+        self.file.close()
 
     def on_create(self, world: World) -> None:
         self.ball = world.global_states[self.ball_key]
@@ -23,18 +28,16 @@ class DataCollector(GameObject):
         self.counter += frame.dt
         if self.counter > INTERVAL:
             self.counter = 0
-            with open(self.filename, 'a', newline='') as f:
-                ball_x, ball_y = self.ball.body.position
-                ball_vx, ball_vy = self.ball.body.velocity
-                _, paddle_y = self.paddle.body.position
+            ball_x, ball_y = self.ball.body.position
+            ball_vx, ball_vy = self.ball.body.velocity
+            _, paddle_y = self.paddle.body.position
 
-                keystate = pygame.key.get_pressed()
-                if keystate[pygame.K_w]:    movement = "up"
-                elif keystate[pygame.K_s]:  movement = "down"
-                else:                       movement = "no"
+            keystate = pygame.key.get_pressed()
+            if keystate[pygame.K_w]:    movement = "up"
+            elif keystate[pygame.K_s]:  movement = "down"
+            else:                       movement = "no"
 
-                writer = csv.writer(f)
-                writer.writerow(
-                    [int(ball_x), int(ball_y), 
-                     int(ball_vx), int(ball_vy), 
-                     int(paddle_y), movement])
+            self.writer.writerow(
+                [int(ball_x), int(ball_y), 
+                    int(ball_vx), int(ball_vy), 
+                    int(paddle_y), movement])
